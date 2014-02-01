9e95412ea2221991bc09eb518b81588445f112aa
Octav <zahariao@gmail.com>
2014-02-01 19:31:01 +0200

    site first version
    added login draft
    register

---
+++ Server/planner/src/planner/routes/home.clj
@@ -0,0 +1,15 @@
+(ns planner.routes.home
+  (:use compojure.core)
+  (:require [planner.views.layout :as layout]
+            [planner.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
