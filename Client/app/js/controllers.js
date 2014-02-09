cee4c956f4505b698f5af51f77edb7f8ee2d2a38
CSG (think8) <cipx@hotmail.com>
2014-02-09 17:49:11 +0200

    remove the global error message

--- Client/app/js/controllers.js
+++ Client/app/js/controllers.js
@@ -10,29 +10,19 @@ angular.module('myApp.controllers', [])
 
 }])
 .controller('RegisterCtrl', ['$scope', '$location', function ($scope, $location) {
+
     $scope.title = 'Welcome';
+
     $scope.update = function (user) {
         $scope.master = angular.copy(user);
 
         if (!$scope.registerForm.$valid)
         {
-            showError("Invalid inputs");
             return;
         }
-        //
-        updateTitle();
 
         $location.path('/RegisterOk');
     };
-
-    function showError(msg) {
-        $scope.hasError = true;
-        $scope.ErrorMessage = msg;
-    }
-
-    function updateTitle() {
-        $scope.title = 'Welcome ' + $scope.master.Email;
-    }
 
 }])
 .controller('RegisterOkCtrl', [function () {
