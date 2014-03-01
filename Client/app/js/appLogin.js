b549baee08ada622188ff1e432bcbabffc8c5235
Andrei <andrei.mandru@gmail.com>
2014-03-01 14:05:52 -0800

    added register link to login page. it points to the main client app at login.html#/register

--- Client/app/js/appLogin.js
+++ Client/app/js/appLogin.js
@@ -1,15 +1,4 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('myAppLogin', [
-  'myAppLogin.controllersLogin'
-]);
-
-angular.module('myAppLogin.controllersLogin', [])
-.controller('LoginCtrl', ['$scope', '$location', function ($scope, $location) {
-    $scope.title = 'Login';
-    $scope.goNext = function (navPath) {
-        $location.path(navPath);
-    }
-}]);
+// App for login
+angular.module('myAppLogin', []);
