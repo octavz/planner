4b0ad036e6cca6ed1037a21a564b313ad99d3435
Ciprian Grigore <cgrigore@gmail.com>
2014-03-24 09:24:27 +0200

    ...

--- Client/app/js/controllers/appbootstrap.js
+++ Client/app/js/controllers/appbootstrap.js
@@ -13,17 +13,6 @@ angular.module('myApp.controllers')
 
         $scope.simulateLogin = function() {
             Auth.login("dummy");
-
         }
-    }
-])
-
-.controller('LogoutCtrl', ['Auth', '$scope', '$location',
-    function(Auth, $scope, $location) {
-        Auth.logout();
-
-        //todo cip ! how do we hide all the screen on logout?
-        //todo cip dont forgot to clear everything on logout
-        $location.path('/Exit');
     }
 ]);
--- Client/app/js/controllers/logout.js
+++ Client/app/js/controllers/logout.js
@@ -8,6 +8,8 @@ angular.module('myApp.controllers')
     function(Auth, $scope, $location) {
         Auth.logout();
 
+        //todo cip ! how do we hide all the screen on logout?
+        //todo cip dont forgot to clear everything on logout
         $location.path('/Exit');
     }
 ]);
