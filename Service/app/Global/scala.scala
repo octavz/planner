7a1dba2974d6b926b63d3961b94be7e645191b6f
Octav <zahariao@gmail.com>
2014-06-08 18:04:14 +0300

    clean

--- Service/app/Global/scala.scala
+++
@@ -1,7 +0,0 @@
-import modules._
-import play.api.GlobalSettings
-import scaldi.play.ScaldiSupport
-
-object Global extends GlobalSettings with ScaldiSupport {
-  def applicationModule = new MainModule :: new UserModule
-}
