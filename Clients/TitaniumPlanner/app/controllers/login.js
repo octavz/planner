5dae6d916023e49e3d6c60a022e4fe95e4021407
ciprian grigore <cgrigore@gmail.com>
2015-04-20 17:52:13 +0300

    integrate login with real server

--- Clients/TitaniumPlanner/app/controllers/login.js
+++ Clients/TitaniumPlanner/app/controllers/login.js
@@ -19,41 +19,52 @@ function login() {
 	var userName = $.txtUserName.value;
 	var userPwd = $.txtUserPassword.value;
 
-	var url = "https://rt-pay.com/vrtwebapi/api/user/login";
-	url = url + "?username=" + userName + "&password=" + userPwd + "&customerNumber=100";
+	var url = "http://system.ml:9000/api/login";
 
 	$.activityIndicator.show();
 	$.btnLogin.enabled = false;
+	
 	var xhr = Ti.Network.createHTTPClient({
 		onload : function(e) {
 
+			Ti.API.log(this.responseText);
 			$.activityIndicator.hide();
 			$.btnLogin.enabled = true;
-			
+
 			var data = JSON.parse(this.responseText);
 			Ti.API.log(this.responseText);
 
 			// navigate to startup ui.
 			var startupView = Alloy.createController('main').getView();
 			startupView.open();
-			
+
 			//$.index.close();
 			//$.index = null;
 		},
 		onerror : function(e) {
 			$.activityIndicator.hide();
 			$.btnLogin.enabled = true;
-			
-			if(this.status == '401'){
-				alert('Wrong credentials!');	
-			}else{
+
+			Ti.API.log(this.responseText);
+			Ti.API.log(this);
+			if (this.status == '401') {
+				alert('Wrong credentials!');
+			} else {
 				alert('Server side issue');
 			}
 		},
 		timeout : 15000 /* in milliseconds */
 	});
-	xhr.open("GET", url);
-	xhr.send();
+	xhr.open("POST", url);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.setRequestHeader('Accept', 'application/json');
+	xhr.send({
+		username : userName,
+		password : userPwd,
+		client_id : "1",
+		grant_type : "password",
+		client_secret : "secret"
+	});
 }
 
 $.login.open();
@@ -64,9 +75,9 @@ $.login.addEventListener('close', function() {
 	$.destroy();
 });
 
-var postLayoutCallback  = function(e){
-  $.login.removeEventListener('postlayout', postLayoutCallback);
-  $.login.activity.actionBar.hide();
+var postLayoutCallback = function(e) {
+	$.login.removeEventListener('postlayout', postLayoutCallback);
+	$.login.activity.actionBar.hide();
 };
 
 $.login.addEventListener('postlayout', postLayoutCallback);
