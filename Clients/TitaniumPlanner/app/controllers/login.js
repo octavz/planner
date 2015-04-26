2eaae4a006a9d922a757c78bbcf5cc86da68fb0f
ciprian grigore <cgrigore@gmail.com>
2015-04-26 16:10:35 +0300

    Refactor login - move http calls into a service library

--- Clients/TitaniumPlanner/app/controllers/login.js
+++ Clients/TitaniumPlanner/app/controllers/login.js
@@ -1,9 +1,11 @@
 // Reference to the singleton
 var userLoginData = Alloy.Models.loginData;
 userLoginData.set('userName', 'Andrei');
+userLoginData.set('userPassword', 'Andrei');
 userLoginData.fetch();
 
 var validation = require('de.mwfire.validate');
+var auth = require('services/auth');
 
 function tryLogin() {
 	var isValid = validation.validate([$.txtUserName, $.txtUserPassword], {
@@ -15,56 +17,38 @@ function tryLogin() {
 	}
 }
 
+function onok() {
+	$.activityIndicator.hide();
+	$.btnLogin.enabled = true;
+	// navigate to startup ui.
+	var startupView = Alloy.createController('ProjectList').getView();
+	startupView.open();
+
+	// $.index.close();
+	//$.index = null;
+}
+
+function onerror(statuscode) {
+	$.activityIndicator.hide();
+	$.btnLogin.enabled = true;
+
+	Ti.API.log(this.responseText);
+	Ti.API.log(this);
+	if (statuscode == '401') {
+		alert('Wrong credentials!');
+	} else {
+		alert('Server side issue');
+	}
+}
+
 function login() {
 	var userName = $.txtUserName.value;
 	var userPwd = $.txtUserPassword.value;
 
-	var url = Alloy.CFG.App.LoginUrl;
-
 	$.activityIndicator.show();
 	$.btnLogin.enabled = false;
 
-	var xhr = Ti.Network.createHTTPClient({
-		onload : function(e) {
-
-			Ti.API.log(this.responseText);
-			$.activityIndicator.hide();
-			$.btnLogin.enabled = true;
-
-			var data = JSON.parse(this.responseText);
-			Ti.API.log(this.responseText);
-
-			// navigate to startup ui.
-			var startupView = Alloy.createController('main').getView();
-			startupView.open();
-
-			//$.index.close();
-			//$.index = null;
-		},
-		onerror : function(e) {
-			$.activityIndicator.hide();
-			$.btnLogin.enabled = true;
-
-			Ti.API.log(this.responseText);
-			Ti.API.log(this);
-			if (this.status == '401') {
-				alert('Wrong credentials!');
-			} else {
-				alert('Server side issue');
-			}
-		},
-		timeout : 15000 /* in milliseconds */
-	});
-	xhr.open("POST", url);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.setRequestHeader('Accept', 'application/json');
-	xhr.send({
-		username : userName,
-		password : userPwd,
-		client_id : "1",
-		grant_type : "password",
-		client_secret : "secret"
-	});
+	auth.Login(userName, userPwd, onok, onerror);
 }
 
 $.login.open();
