6fbb5416331f5d80d99c7871a3693d2ffe0b8bdd
ciprian grigore <cgrigore@gmail.com>
2015-05-03 15:39:01 +0300

    Project List Screen: move the new project section to Project New Screen.

--- Clients/TitaniumPlanner/app/controllers/ProjectList.js
+++ Clients/TitaniumPlanner/app/controllers/ProjectList.js
@@ -1,27 +1,5 @@
 var args = arguments[0] || {};
 
-$.btnInsert.addEventListener('click', function(e) {
-	//Hides the keyboard in iOS
-	$.tfInsert.blur();
-
-	//if there is something in the textbox
-	if ($.tfInsert.value != "") {
-		//This is how we are creating an instance of a model
-		var project = Alloy.createModel("Project", {
-			//with out custom parameters
-			name : $.tfInsert.value,
-			public : true
-		});
-
-		//This is how we save a model to our databaseif the model already exists, the save will be an "update".
-		project.save();
-
-		//Resets the model's state from the database
-		Alloy.Collections.Project.fetch();
-	} else {
-		alert("Please fill out the text field above!");
-	}
-});
 
 //if we do a longpress we can delete the row
 $.tbProjects.addEventListener('longpress', function(e) {
