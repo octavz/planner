cf5262c770f165cd558497e1627522bd28864f27
Andrei Mandru <andrei.mandru@gmail.com>
2015-05-09 13:06:04 +0300

    New/Edit Project: add save to action bar + small UI changes

--- Clients/TitaniumPlanner/app/controllers/ProjectEdit.js
+++ Clients/TitaniumPlanner/app/controllers/ProjectEdit.js
@@ -9,6 +9,46 @@ if (project == null) {
 } else {
 	Ti.API.log(project);
 	$.win.title = "Edit: " + project.attributes["name"];
+	//TODO: Shouldn't we use binding?
 	$.tfProjectName.value = project.attributes["name"];
 	$.tfProjectDesc.value = project.attributes["desc"];
 }
+
+function SaveProject(e) {
+	Ti.API.log("info", "[Info] Save project with Id: " + projectId.toString());
+	
+	//TODO: Is there a way of automate validation?
+	if(validate() === true){	 
+		project.save();
+	}else{
+		alert('Please fill all fields!');
+	}
+}
+
+function validate(){
+	var isValid = true;
+	
+	if($.tfProjectName.value.lenth == 0){
+		isValid = false;
+	}
+	
+	if($.tfProjectDesc.value.lenth == 0){
+		isValid = false;
+	}
+	
+	return isValid;
+}
+
+$.win.addEventListener("open", function() {
+
+	$.win.activity.onCreateOptionsMenu = function(e) {
+		
+		e.menu.add({
+			title : "Save",
+			icon : Ti.Android.R.drawable.ic_menu_save,
+			showAsAction : Titanium.Android.SHOW_AS_ACTION_IF_ROOM,
+		}).addEventListener("click", SaveProject);
+	};
+
+	$.win.activity.invalidateOptionsMenu();
+});
--- Clients/TitaniumPlanner/app/controllers/ProjectNew.js
+++ Clients/TitaniumPlanner/app/controllers/ProjectNew.js
@@ -1,25 +1,41 @@
 var args = arguments[0] || {};
 
-$.btnInsert.addEventListener('click', function(e) {
-	//Hides the keyboard in iOS
-	$.tfInsert.blur();
+function SaveNewProject(e) {
+	Ti.API.log("info", "[Info] Save New Project!");
+	
+	// Hides the keyboard in iOS
+	$.tfProjectName.blur();
 
-	//if there is something in the textbox
-	if ($.tfInsert.value != "") {
-		//This is how we are creating an instance of a model
+	// if there is something in the textbox
+	if ($.tfProjectName.value != "") {
+		// This is how we are creating an instance of a model
 		var project = Alloy.createModel("Project", {
 			//with out custom parameters
-			name : $.tfInsert.value,
+			name : $.tfProjectName.value,
 			public : true
 		});
 
-		//This is how we save a model to our databaseif the model already exists, the save will be an "update".
+		// This is how we save a model to our databaseif the model already exists, the save will be an "update".
 		project.save();
 
-		//Resets the model's state from the database
+		// Resets the model's state from the database
 		Alloy.Collections.Project.fetch();
 	} else {
 		alert("Please fill out the text field above!");
 	}
+}
+
+$.win.addEventListener("open", function() {
+
+	$.win.activity.onCreateOptionsMenu = function(e) {
+		
+		e.menu.add({
+			title : "Save",
+			icon : Ti.Android.R.drawable.ic_menu_save,
+			showAsAction : Titanium.Android.SHOW_AS_ACTION_IF_ROOM,
+		}).addEventListener("click", SaveNewProject);
+	};
+
+	$.win.activity.invalidateOptionsMenu();
 });
 
