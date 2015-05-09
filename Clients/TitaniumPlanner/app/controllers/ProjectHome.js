7509beb05b653d48d923db09e4254a532e6fcee5
ciprian grigore <cgrigore@gmail.com>
2015-05-09 16:47:56 +0300

    Add menu in project home screen

--- Clients/TitaniumPlanner/app/controllers/ProjectHome.js
+++ Clients/TitaniumPlanner/app/controllers/ProjectHome.js
@@ -12,3 +12,17 @@ if (project == null) {
 	Ti.API.log(project);
 	$.win.title = project.attributes["name"] + " Home";
 }
+
+function OpenEditProject(e) {
+	Ti.API.log("projectId", projectId);
+	Alloy.createController('ProjectEdit', {
+		projectId : projectId
+	}).getView().open();
+}
+
+function OpenDeleteProject(e) {
+	Ti.API.log("projectId", projectId);
+	Alloy.createController('ProjectEdit', {
+		projectId : projectId
+	}).getView().open();
+}
